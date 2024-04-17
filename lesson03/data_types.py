3641e23baca2f4dfc9b380d60feb98d2398d16fa
Boominathan <eboominathan@gmail.com>
2024-04-17 07:38:44 +0530

    datatypes completed

---
+++ lesson03/data_types.py
@@ -0,0 +1,30 @@
+# casting
+
+# decade = 1980
+# print(type(decade))
+# print(decade)
+
+# statement = "I like rock m  usic from "+str(decade)+"S"
+# print(statement)
+
+
+# Build a menu
+
+# title = "menu".upper()
+# print(title.center(20, "="))
+# print("Coffee".ljust(16, '.')+"$1".rjust(4))
+# print("Muffin".ljust(16, '.')+"$2".rjust(4))
+# print("Cheesecake".ljust(16, '.')+"$4".rjust(4))
+
+# string index value
+first = "test"
+# print(first[1])
+# print(first[-1])
+# print(first[1:-1])
+# print(first[1:])
+
+# some method to return boolean
+
+print(first.startswith("t"))
+print(first.endswith("s"))
+print(tuple(first))
--- lessson01/main.py
+++ lessson01/main.py
@@ -1,6 +1,6 @@
-line1="***************"
-line2="*             *"
-line3="**  WELCOME  **"
+line1 = "***************"
+line2 = "*             *"
+line3 = "**  WELCOME  **"
 
 print('')
 print(line1)
@@ -10,3 +10,5 @@ print(line2)
 print(line1)
 print('')
 
+name = input("Enter your name")
+print("Hi", name)
